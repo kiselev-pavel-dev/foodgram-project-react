87dc9e20659d67db00f0ece9832f0ec9c4a7ba59
Павел Киселев <neznika2@yandex.ru>
2022-08-02 19:15:01 +0300

    Deploy

--- backend/api/views.py
+++ backend/api/views.py
@@ -40,7 +40,7 @@ class UserViewSet(ReadOnlyModelViewSet):
 
 class SubscriptionViewSet(ModelViewSet):
     serializer_class = SubscriptionSerializer
-    pagination_class = PageNumberPagination
+    pagination_class = None
     permission_classes = (permissions.IsAuthenticated,)
 
     def get_queryset(self):
